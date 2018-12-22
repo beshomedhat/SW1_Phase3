0547d12217dec4d83041af42f3e236a1eced48dc
BeshoyBoctor <BeshoyM2015@gmail.com>
2018-12-22 15:56:51 +0200

    Update Searching.java
    add getCategory fuction

--- assi_sw1/src/assi_sw1/Searching.java
+++ assi_sw1/src/assi_sw1/Searching.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package assi_sw1;
 
-/**
- *
- * @author Poula
- */
-public class Searching {
+public class Searching extends searcher
+ {
+  public static void getCategory (String filename,String searchStr) throws FileNotFoundException{
+
+        String searchCat = "";
+           System.out.print("Enter Category of item : ");
+           System.out.println();
+           System.out.print("For Wallet write 'Wallets ");
+           System.out.println();
+           System.out.print("For IDs write 'ID' ");
+           System.out.println();
+           System.out.print("For Phones write 'Phones' ");
+           System.out.println();
+           System.out.print("For Papers&Files write p and f ");
+           System.out.println();
+           Scanner scan = new Scanner(new File(searchCat));
+            while(scan.hasNext()){
+            String line0 = scan.nextLine().toLowerCase().toString();
+            if(line0.contains(searchStr)){
+            System.out.println(line0);}
+        
+    }
     
+ }
+     public static void getItemFeatures (String filename,String searchStr) throws FileNotFoundException{
+            String searchCat = "";
+            System.out.print("Enter Features of item : ");
+            Scanner scan = new Scanner(new File(searchCat));
+            while(scan.hasNext()){
+            String line0 = scan.nextLine().toLowerCase().toString();
+            if(line0.contains(searchStr)){
+                System.out.println(line0);
+            
+   
 }
